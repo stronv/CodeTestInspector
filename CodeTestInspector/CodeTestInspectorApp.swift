b093b38fe7eb18e4774d44c0eaa03e420b7b1ee6
Artem Tabachenko <fexo5467@gmail.com>
2025-04-20 14:05:14 +0300

    Initial Commit

---
+++ CodeTestInspector/CodeTestInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTestInspectorApp.swift
+//  CodeTestInspector
+//
+//  Created by Artyom Tabachenko on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeTestInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
